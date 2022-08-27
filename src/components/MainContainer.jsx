8c6fed2f9cfade87de5506774cbb17be36f64009
An7112 <thanhan7112@gmail.com>
2022-08-27 10:39:33 +0700

    scrollLeft

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeContainer from './HomeContainer'
 import { motion } from 'framer-motion'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import RowContainer from './RowContainer'
+import {useStateValue} from '../context/StateProvider'
 const MainContainer = () => {
+  const [{foodItems}, dispatch] = useStateValue()
+  const LenghtItems = foodItems?.length
+  const scrollLength = LenghtItems * 205 + LenghtItems * 5
+  const [scrollValue, setScrollValue] = useState(0)
+  console.log(scrollLength)
+  console.log(scrollValue)
+  useEffect(() => {}, [scrollValue])
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center'>
       <HomeContainer />
@@ -11,15 +19,18 @@ const MainContainer = () => {
         <div className='w-full flex items-center justify-between'>
           <p className='text-2xl font-semibold uppercase relative text-headingColor before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>Our fresh & healthy fruits</p>
           <div className='hidden md:flex gap-3 items-center'>
-            <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center'>
+            <motion.div onClick={() => setScrollValue(scrollValue-285)} whileTap={{ scale: 0.75 }} className={`w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg ${scrollValue === 0 ? 'hidden' : 'flex'} items-center justify-center`}>
               <MdChevronLeft className='text-lg text-white' />
             </motion.div>
-            <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center'>
+            <motion.div  onClick={() => setScrollValue(scrollValue+285)} whileTap={{ scale: 0.75 }} className={`w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg ${scrollValue >= scrollLength ? 'hidden' : 'flex'} items-center justify-center`}>
               <MdChevronRight className='text-lg text-white' />
             </motion.div>
           </div>
         </div>
-        <RowContainer flag={true}/>
+        <RowContainer scrollValue={scrollValue} flag={true} data={foodItems?.filter(n => n.category === "fruits")}/>
+      </section>
+      <section className='w-full my-6'>
+        
       </section>
     </div>
   )
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import {MdShoppingBasket} from 'react-icons/md'
+import React, { useEffect, useRef } from 'react'
+import { MdShoppingBasket } from 'react-icons/md'
 import { motion } from 'framer-motion'
-const RowContainer = ({flag}) => {
-  return (
-    <div className={`w-full my-12 ${flag ? 'overflow-x-scroll' : 'overflow-x-hidden'}`}>
-        <div className='w-300 h-auto bg-cardOverlay rounded-lg p-2 md:w-340 my-12 backdrop-blur-lg hover:drop-shadow-lg'>
-            <div className='w-full flex items-center justify-between'>
-                <motion.img whileHover={{scale: 1.2}} src='https://firebasestorage.googleapis.com/v0/b/foodapp-645c6.appspot.com/o/Images%2F1661495524974-c1.png?alt=media&token=a4a31fe7-605b-4886-bc01-a9edb7b5f9e8' alt='' className='w-40 -mt-8 drop-shadow-2xl'/>
-                <motion.div whileTap={{scale: 0.75}} className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
-                    <MdShoppingBasket className='text-white'/>
-                </motion.div>
-            </div>
-            <div className='w-full flex flex-col items-end justify-end'>
-                <p className='text-textColor font-semibold text-base md:text-lg'>Chocolate & Vanilla</p>
-                <p className='mt-1 text-sm text-gray-500'>45 Calories</p>
-                <div className='flex items-center gap-8'>
-                    <p className='text-lg text-headingColor font-semibold'><span className='text-sm text-red-500'>$</span> 5.25</p>
+const RowContainer = ({ flag, data, scrollValue }) => {
+    const rowContainer = useRef();
+    useEffect(() => {
+        rowContainer.current.scrollLeft = scrollValue;
+    }, [scrollValue]);
+    return (
+        <div ref={rowContainer} className={`w-full flex scroll-smooth items-center gap-3 my-12 ${flag ? 'overflow-x-scroll scrollbar-none' : 'overflow-x-hidden'}`}>
+            {data && data.map((item) => (
+                <div key={item.id} className='w-275 h-[175px] min-w-[275px] md:min-w-[300px] bg-cardOverlay rounded-lg py-2 px-4 md:w-300 my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative'>
+                    <div className='w-full flex items-center justify-between'>
+                        <motion.img whileHover={{ scale: 1.2 }} src={item.imageURL} className="w-40 h-full -mt-8 drop-shadow-2xl" />
+                        <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
+                            <MdShoppingBasket className='text-white' />
+                        </motion.div>
+                    </div>
+                    <div className='w-full flex flex-col items-end justify-end absolute bottom-0 right-2'>
+                        <p className='text-textColor font-semibold text-base md:text-lg'>{item.title}</p>
+                        <p className='mt-0 text-sm text-gray-500'>{item.calories} Calories</p>
+                        <div className='flex items-center gap-8'>
+                            <p className='text-lg text-headingColor font-semibold'><span className='text-sm text-red-500'>$</span> {item.price}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
-    </div>
-  )
+    )
 }
 
 export default RowContainer
